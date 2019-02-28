ceacd31739c62bce7aa6aec888b2312fb64da1fb
Stephen Wool <stevewool7@gmail.com>
2019-02-28 15:30:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,11 +16,11 @@ function logWhisper(string) {
 
 
 function sayHiToGrandma(string) {
-  if (string.toLowerCase()) {
+  if(string.toLowerCase()) {
     return "I can't hear you!"
-  } else if (string.toUpperCase()) {
+  } else if(string.toUpperCase()) {
     return "YES INDEED!"
-  } else if (string === "I love you, Grandma") {
+  } else if(string === "I love you, Grandma") {
     return "I love you, too"
   }
   
