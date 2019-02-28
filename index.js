a1ee0687df95518487b20170325c7da11fa064b4
Stephen Wool <stevewool7@gmail.com>
2019-02-28 15:36:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function sayHiToGrandma(string) {
   if (string.toLowerCase()) {
     return "I can't hear you!"
   } else if (string.toUpperCase()) {
-    return "YES INDEED!"
+    return `YES INDEED!`
   } else if (string === "I love you, Grandma") {
     return "I love you, too"
   }
